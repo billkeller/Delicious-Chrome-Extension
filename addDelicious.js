02554d93b8b891a40d84cea94f31e59223e31eae
Bill Keller <bill@billkeller.name>
2009-06-07 18:10:23 +0000

    Title tags now get passed in as the title of your bookmark.

--- addDelicious.js
+++ addDelicious.js
@@ -1,6 +1,6 @@
+var deliciousUrl = "http://delicious.com/save?v=5&amp;noui&amp;jump=close&amp;url=";
 var url;
 var title;
-var deliciousUrl = "http://delicious.com/save?v=5&amp;noui&amp;jump=close&amp;url=";
 
 // grabbed from delicious_content_script.js
 chrome.self.onConnect.addListener(function(port) {
@@ -11,8 +11,8 @@ chrome.self.onConnect.addListener(function(port) {
 });
 
 function add_delicious() {
-    window.open(deliciousUrl + url + ' ','deliciousuiv5','location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550');
+    window.open(deliciousUrl + url + '&title=' + title +' ','deliciousuiv5','location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550');
   };
 
-
+//sample popup URL from Delicious: 
 //javascript:(function(){f='http://delicious.com/save?url='+encodeURIComponent(window.location.href)+'&title='+encodeURIComponent(document.title)+'&v=5&';a=function(){if(!window.open(f+'noui=1&jump=doclose','deliciousuiv5','location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550'))location.href=f+'jump=yes'};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})()
