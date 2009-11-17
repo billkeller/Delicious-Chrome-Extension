3c007b9f921aaea4cea8473e396b33240bed9e06
Bill Keller <bill@billkeller.name>
2009-11-17 12:11:17 +0000

    Removed toolstrip and switched to a toolbar menu using Chromes "browser_action" http://code.google.com/chrome/extensions/browserAction.html
    Next steps:
    Create keyboard shortcut for the "Save new bookmark" menu.
    Use highlighted text in the notes field when saving a bookmark.

--- addDelicious.js
+++ addDelicious.js
@@ -2,17 +2,13 @@ var deliciousUrl = "http://delicious.com/save?v=5&amp;noui&amp;jump=close&amp;ur
 var url;
 var title;
 
-// grabbed from delicious_content_script.js
-chrome.self.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(data) {
-    url = data.url;
-    title = data.title;
-  });
-});
-
-function add_delicious() {
-    window.open(deliciousUrl + url + '&title=' + title +' ','deliciousuiv5','location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550');
-  };
+ function add_delicious() {
+ 	chrome.tabs.getSelected( null , function(tab) {
+ 		url = (tab.url);
+ 		title = (tab.title);
+ 		window.open(deliciousUrl + url + '&title=' + title +' ','deliciousuiv5','location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550');
+ 	});
+ }
 
 //sample popup URL from Delicious: 
 //javascript:(function(){f='http://delicious.com/save?url='+encodeURIComponent(window.location.href)+'&title='+encodeURIComponent(document.title)+'&v=5&';a=function(){if(!window.open(f+'noui=1&jump=doclose','deliciousuiv5','location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550'))location.href=f+'jump=yes'};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})()
