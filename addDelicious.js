7b26f82f0adef860371fe98ccdc794e9117232d1
Mike Matz <mike@pixor.net>
2010-01-19 01:15:12 +0000

    Added "notes" passing (only for keyboard shortcut), cleaned up function names a bit

--- addDelicious.js
+++ addDelicious.js
@@ -7,31 +7,42 @@
 		'keydown', 
 		function(e) {
 			if (e.which == bookmarkKeyCode && e.ctrlKey) {
-				triggerAddDelicious();
+				addDeliciousFromContentScript();
 			}
 		},
 		false
 	);
+	
+	// Trigger delicious window from popup.html
+	// Note: currently no way to retrieve selected text
+	addDeliciousFromPopup = function() {
+		chrome.tabs.getSelected(null, function(tab) {
+			chrome.extension.sendRequest({
+				type: 'addDelicious',
+				url: tab.url,
+				title: tab.title,
+				notes: ''
+			});
+		});
+	};
 
 	// Send request to background page (content script not permitted to create new windows)
-	triggerAddDelicious = function() {
+	addDeliciousFromContentScript = function() {
 		var url = document.location.toString(),
-			title = document.title;
+			title = document.title,
+			notes = '';
 		
-		if (!/^chrome/.test(url)) {
-			// URL seems good; use it
-			chrome.extension.sendRequest({
-				url: url,
-				title: title
-			});
-		} else {
-			// Called from popup.html; need to get current tab
-			chrome.tabs.getSelected(null, function(tab) {
-				chrome.extension.sendRequest({
-					url: tab.url,
-					title: tab.title
-				});
-			});
+		if (window && window.getSelection) {
+			notes = window.getSelection().toString();
+		} else if (document && document.getSelection) {
+			notes = document.getSelection().toString();
 		}
+		
+		chrome.extension.sendRequest({
+			type: 'addDelicious',
+			url: url,
+			title: title,
+			notes: notes
+		});
 	};
 })();
