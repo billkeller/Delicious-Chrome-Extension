bd06cac570f16bc075790174ec44f0d40d766489
Bill Keller <bill@billkeller.name>
2010-01-19 12:27:48 +0000

    adding a branch that contains JUST the keyboard shortcut and JS updates, for testing before merging w/ trunk.

---
+++ addDelicious.js
@@ -0,0 +1,36 @@
+(function() {
+	
+	// Listen for keyboard shortcut
+	var bookmarkKeyCode = 'D'.charCodeAt(0);
+	window.addEventListener(
+		'keydown', 
+		function(e) {
+			if (e.which == bookmarkKeyCode && e.ctrlKey) {
+				triggerAddDelicious();
+			}
+		},
+		false
+	);
+
+	// Send request to background page (content script not permitted to create new windows)
+	triggerAddDelicious = function() {
+		var url = document.location.toString(),
+			title = document.title;
+		
+		if (!/^chrome/.test(url)) {
+			// URL seems good; use it
+			chrome.extension.sendRequest({
+				url: url,
+				title: title
+			});
+		} else {
+			// Called from popup.html; need to get current tab
+			chrome.tabs.getSelected(null, function(tab) {
+				chrome.extension.sendRequest({
+					url: tab.url,
+					title: tab.title
+				});
+			});
+		}
+	};
+})();
