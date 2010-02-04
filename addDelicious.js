9d29532d01631c51153e15b5fff27f75adaf673a
Bill Keller <bill@billkeller.name>
2010-02-04 15:10:50 +0000

    attempting to add user prefs for keyboard shortcut. This is not a working version.

--- addDelicious.js
+++ addDelicious.js
@@ -1,13 +1,24 @@
-
 (function() {
-	
-	console.log('loaded addDelicious');
-	
-	// Listen for keyboard shortcut
-	var bookmarkKeyCode = 'D'.charCodeAt(0);
+	function getVariableFromLocalStorage(variableName,defaultValue) {
+		this[variableName] = defaultValue;
+		var onResponse = function (response) {
+			 if (response != null) {
+				this[variableName] = response;
+				console.log(variableName + ' is set to: ' + response);
+			 } else {
+				console.log(variableName + ' not found in localStorage!  ' + response);
+		 }
+		}
+		chrome.extension.sendRequest({'action': 'getFromLocalStorage', 'variableName': variableName}, onResponse)
+	};
+
+	// Listen for key press
 	window.addEventListener(
-		'keydown', 
+		'keydown',
 		function(e) {
+			// bookmarkKeyCode is always returning "undefined" not sure how to grab it from getVariableFromLocalStorage function
+			var bookmarkKeyCode = getVariableFromLocalStorage('bookmarkKeyCode','D');
+			console.log(bookmarkKeyCode);
 			if (e.which == bookmarkKeyCode && e.altKey) {
 				console.log('pressed ^d');
 				addDeliciousFromContentScript();
@@ -15,22 +26,6 @@
 		},
 		false
 	);
-	
-	// Trigger delicious window from popup.html
-	// Note: currently no way to retrieve selected text
-	// addDeliciousFromPopup is now set directly in popup.html
-	/*
-	addDeliciousFromPopup = function(text) {
-		chrome.tabs.getSelected(null, function(tab) {
-			chrome.extension.sendRequest({
-				type: 'addDelicious',
-				url: tab.url,
-				title: tab.title,
-				notes: text
-			});
-		});
-	};
-	*/
 
 	// Send request to background page (content script not permitted to create new windows)
 	addDeliciousFromContentScript = function() {
@@ -38,17 +33,17 @@
 			title = document.title,
 			notes = '',
 			notesMaxLength = 1000;
-		
+
 		if (window && window.getSelection) {
 			notes = window.getSelection().toString();
 		} else if (document && document.getSelection) {
 			notes = document.getSelection().toString();
 		}
-		
+
 		if (notes && notes.length > notesMaxLength) {
 			notes = notes.substring(0, notesMaxLength - 1);
 		}
-		
+
 		chrome.extension.sendRequest({
 			type: 'addDelicious',
 			url: url,
