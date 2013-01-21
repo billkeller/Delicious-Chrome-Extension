5aa261ce356f943d3b218f45dce757c44d869b93
Bill keller <bill@billkeller.name>
2013-01-21 08:27:29 -0500

    Version 1.6 manifest V.2 upgrade, works with delicious.com redesign

--- addDelicious.js
+++ addDelicious.js
@@ -1,66 +1,107 @@
+var localSettings = {
+	bookmarkKeyChar: 'D',
+	bookmarkSpecialKey: 'alt'
+};
 
-	var localSettings = {
-		bookmarkKeyChar: 'D',
-		bookmarkSpecialKey: 'alt'
-	};
-
-	function getVariableFromLocalStorage(variableName,defaultValue) {
-		localSettings[variableName] = defaultValue;
-		var onResponse = function (response) {
-			 if (response !== null) {
-				localSettings[variableName] = response;
-				// console.log('localSettings.' + variableName + ' is set to: ' + response);
-			 } else {
-				// console.log('localSettings.' + variableName + ' not found in localStorage!  ' + response);
-			}
+function getVariableFromLocalStorage(variableName,defaultValue) {
+	localSettings[variableName] = defaultValue;
+	var onResponse = function (response) {
+		 if (response !== null) {
+			localSettings[variableName] = response;
+			// console.log('localSettings.' + variableName + ' is set to: ' + response);
+		 } else {
+			// console.log('localSettings.' + variableName + ' not found in localStorage!  ' + response);
 		}
-		chrome.extension.sendRequest({'action': 'getFromLocalStorage', 'variableName': variableName}, onResponse)
-	};
+	}
+	chrome.extension.sendRequest({'action': 'getFromLocalStorage', 'variableName': variableName}, onResponse)
+};
 
-	getVariableFromLocalStorage('bookmarkKeyChar', localSettings.bookmarkKeyChar);
-	getVariableFromLocalStorage('bookmarkSpecialKey', localSettings.bookmarkSpecialKey);
+getVariableFromLocalStorage('bookmarkKeyChar', localSettings.bookmarkKeyChar);
+getVariableFromLocalStorage('bookmarkSpecialKey', localSettings.bookmarkSpecialKey);
 
-	// Listen for key press
-	window.addEventListener(
-		'keydown',
-		function(e) {
-			if ((e.which == localSettings.bookmarkKeyChar.charCodeAt(0))
-					&& (
-						(localSettings.bookmarkSpecialKey == 'alt' && e.altKey)
-						|| (localSettings.bookmarkSpecialKey == 'ctrl' && e.ctrlKey)
-						|| (localSettings.bookmarkSpecialKey == 'meta' && e.metaKey)
-					)) {
-				// console.log('pressed '+ localSettings.bookmarkSpecialKey + ' ' + localSettings.bookmarkKeyChar);
-				// prevent the default, doesn't seem to overwrite the windows (Meta) key
-				e.preventDefault();
-				addDeliciousFromContentScript();
-			}
-		},
-		false
-	);
+// Listen for key press
+window.addEventListener(
+	'keydown',
+	function(e) {
+		if ((e.which == localSettings.bookmarkKeyChar.charCodeAt(0))
+				&& (
+					(localSettings.bookmarkSpecialKey == 'alt' && e.altKey)
+					|| (localSettings.bookmarkSpecialKey == 'ctrl' && e.ctrlKey)
+					|| (localSettings.bookmarkSpecialKey == 'meta' && e.metaKey)
+				)) {
+			// console.log('pressed '+ localSettings.bookmarkSpecialKey + ' ' + localSettings.bookmarkKeyChar);
+			// prevent the default, doesn't seem to overwrite the windows (Meta) key
+			e.preventDefault();
+			addDeliciousFromContentScript();
 
-	// Send request to background page (content script not permitted to create new windows)
-	// This one is running!
-	addDeliciousFromContentScript = function() {
-		var url = document.location.toString(),
-			title = document.title,
-			notes = '',
-			notesMaxLength = 1000;
+		    /*
+		    Optionally, we COULD run the bookmarklet itself, but only when initiated via keyboard shortcut
+		    function f(i) {
+		        if (i.origin !== e + "" + n && i.origin !== t + "" + n) {
+		            return
+		        }
+		        if (i.data === "destroy_bookmarklet") {
+		            var s = document.getElementById(r);
+		            if (s) {
+		                document.body.removeChild(s);
+		                s = null
+		            }
+		        }
+		    }
+		    var e = "http://",
+		        t = "https://",
+		        n = "delicious.com",
+		        r = "DELI_bookmarklet_iframe",
+		        i = document.getElementById(r);
+		    if (i) {
+		        return
+		    }
+		    var s = e + "" + n + "/save?",
+		        o = document.createElement("iframe");
+		    o.id = r;
+		    o.src = s + "url=" + encodeURIComponent(window.location.href) + "&title=" + encodeURIComponent(document.title) + "&note=" + encodeURIComponent("" + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text)) + "&v=1.1";
+		    o.style.position = "fixed";
+		    o.style.top = "0";
+		    o.style.left = "0";
+		    o.style.height = "100%";
+		    o.style.width = "100%";
+		    o.style.zIndex = "9999999";
+		    o.style.border = "none";
+		    o.style.visibility = "hidden";
+		    o.onload = function () {
+		        this.style.visibility = "visible"
+		    };
+		    document.body.appendChild(o);
+		    var u = window.addEventListener ? "addEventListener" : "attachEvent";
+		    var a = u == "attachEvent" ? "onmessage" : "message";
+		    window[u](a, f, false)
+			*/
 
-		if (window && window.getSelection) {
-			notes = window.getSelection().toString();
-		} else if (document && document.getSelection) {
-			notes = document.getSelection().toString();
 		}
+	},
+	false
+);
 
-		if (notes && notes.length > notesMaxLength) {
-			notes = notes.substring(0, notesMaxLength - 1);
-		}
-		chrome.extension.sendRequest({
-			type: 'addDelicious',
-			url: url,
-			title: title,
-			notes: notes
-		});
-	};
+// Send request to background page (content script not permitted to create new windows)
+addDeliciousFromContentScript = function() {
+	var url = document.location.toString(),
+		title = document.title,
+		notes = '',
+		notesMaxLength = 1000;
 
+	if (window && window.getSelection) {
+		notes = window.getSelection().toString();
+	} else if (document && document.getSelection) {
+		notes = document.getSelection().toString();
+	}
+
+	if (notes && notes.length > notesMaxLength) {
+		notes = notes.substring(0, notesMaxLength - 1);
+	}
+	chrome.extension.sendRequest({
+		type: 'addDelicious',
+		url: url,
+		title: title,
+		notes: notes
+	});
+};
--- background.js
+++ background.js
@@ -4,22 +4,22 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 function addDeliciousFromPopup(text){
 	chrome.tabs.query({
-		    active: true,                              // Select active tabs
-		    windowId: chrome.windows.WINDOW_ID_CURRENT, // In the current window
+			active: true,                              // Select active tabs
+			windowId: chrome.windows.WINDOW_ID_CURRENT, // In the current window
 
 		}, function(array_of_Tabs) {
-		    // Since there can only be one active tab in one active window, 
-		    //  the array has only one element
-		    var tab = array_of_Tabs[0],
-		    	url = tab.url,
-		    	title = tab.title,
-		    	notes = text,
-		    	notesMaxLength = 1000;
+			// Since there can only be one active tab in one active window, 
+			//  the array has only one element
+			var tab = array_of_Tabs[0],
+				url = tab.url,
+				title = tab.title,
+				notes = text,
+				notesMaxLength = 1000;
 			if (notes && notes.length > notesMaxLength) {
 				notes = notes.substring(0, notesMaxLength - 1);
 			}					
-		    
-		    addDelicious({
+			
+			addDelicious({
 				url: url,
 				title: title,
 				notes: notes
@@ -27,67 +27,56 @@ function addDeliciousFromPopup(text){
 		});
 };
 
+chrome.webRequest.onCompleted.addListener(
+	function(details){
+		// We're only here IF the XHR request is the URL found under filters
+		// console.log("resource", details.url);
+		// we also need to make sure this is OUR popup, as opposed to the live delicious.com
+		chrome.tabs.query({
+			active: true,                              // Select active tabs
+			windowId: chrome.windows.WINDOW_ID_CURRENT, // In the current window
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	
-	if(changeInfo.status == "loading") {
-	
-		chrome.windows.getCurrent(function(w) {
-			chrome.tabs.getSelected(w.id,
-			function (response){
-				// alert('ourWindow: ' + ourWindow);
-				// alert('response id: '+ response.id);
-				if(response.id == ourWindow) {
-					// You're here because this is the ID of the popup window our extension created
-					// alert('Found this puppy');
-		
-					if (typeof String.prototype.startsWith != 'function') {
-					  String.prototype.startsWith = function (str){
-						return this.slice(0, str.length) == str;
-					  };
-					}      
-			
-					var data = tab.url
-					var tldURL = 'http://delicious.com'
-					var saveURL = 'http://delicious.com/save?'
-					var registerURL = 'http://delicious.com/register'
-					var loginURL = 'http://delicious.com/login'
-					if(data.startsWith(saveURL) || data.startsWith(registerURL) || data.startsWith(loginURL)) {
-						// alert('Do not close our window');
-						if(data.startsWith(registerURL)) {
-							// If it's the register page, push them to the login page
-							// alert('register page');
-							chrome.tabs.update(tabId, {url: "http://delicious.com/login"});
-							// we edit the layout of the login page a bit in style.css
-						}
-					} else {
-						// Double check that it's actually our popup, if not, exit
-						// What I'm founding now, is that windows CAN and DO share the same tab.id
-						// 
-						if(data.startsWith(tldURL)) {
-							chrome.tabs.remove(tabId);
-						} else {
-							// We're here by mistake
-							// alert('Whoops, don\'t close ME!');
-						}
-					}
-				}
-			
-			
-			});
-		});	
-	
+		}, function(array_of_Tabs) {
+			// Since there can only be one active tab in one active window, 
+			//  the array has only one element
+			var tab = array_of_Tabs[0],
+				url = tab.url,
+				title = tab.title,
+				tabId = tab.id,
+				windowId = chrome.windows.WINDOW_ID_CURRENT;
+			checkPopup(tab,url,tabId, windowId);
+		});		
+	},
+	// filters
+	{
+		urls:[
+			"*://*.delicious.com/api/v1/posts/addoredit*",
+			"*://*.delicious.com/api/v1/posts/delete*"
+		]
 	}
-	
-});
+);
+
+function checkPopup(tab,url,tabId) {
+	// console.log('from Delicious Tools popup, ourWindow id: ' + ourWindow + ', our popupId:' + tabId);
+	if (ourWindow == tabId){
+		chrome.tabs.remove(tabId);
+	};
+};
+
+function getOurWindow(){
+	chrome.tabs.getSelected(window.id,
+		function (response){
+			ourWindow = response.id
+			// console.log('The window Delicious Tools created has an id of: ' + ourWindow);
+		});
+};
 
 chrome.extension.onRequest.addListener(
 	function(message, sender, sendResponse) {
-		console.log('I heard something');
 		if (message.action && sender.tab) {
 			switch (message.action) {
 				case 'getFromLocalStorage':
-					console.log(message.variableName + ': ' + localStorage [message.variableName]);
+					// console.log(message.variableName + ': ' + localStorage [message.variableName]);
 					if (localStorage[message.variableName] !== null) {
 						sendResponse(localStorage[message.variableName]);
 					}
@@ -97,7 +86,6 @@ chrome.extension.onRequest.addListener(
 	}
 );
 
-
 var selection_callbacks = [];
 function getSelection(callback) {
 	selection_callbacks.push(callback);
@@ -105,14 +93,20 @@ function getSelection(callback) {
 };
 
 chrome.extension.onRequest.addListener(function (request) {
-	// this listener is activate be it from shortcut or be it from popup
+	// this listener is activated be it from shortcut or be it from popup
 	var callback = selection_callbacks.shift();
-	callback(request);
+	// console.log(callback);
+	// only our addDeliciousFromPopup needs this, otherwise it's undefined
+	// There's probably a better way to do this...
+	if (typeof callback != 'undefined') {
+		callback(request);
+	}
 });
 
-// Respond to messages passed from addDelicous.js content script
+// Main request listener
 chrome.extension.onRequest.addListener(
 	function(request, sender, sendResponse) {
+		// console.log('listening');
 		if (request.type === 'addDelicious') {
 			addDelicious(request);
 		}
@@ -126,13 +120,12 @@ addDelicious = function(conf) {
 		url = c.url || doc.location,
 		title = c.title || doc.title,
 		notes = c.notes || '',
-		w = c.width || 550,
-		h = c.height || 550,
+		w = c.width || 500,
+		h = c.height || 430,
 		deliciousUrl = c.deliciousUrl || "http://delicious.com/save?url=",
 		fullUrl;
 
-	fullUrl = deliciousUrl + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + '&note=' + encodeURIComponent(notes);
-	console.log('addDelicious has run');
+	fullUrl = deliciousUrl + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + '&note=' + encodeURIComponent(notes) + '&v=1.1';
 	// Chrome's API for creating windows (rather than simple window.open)
 	chrome.windows.create({
 		url: fullUrl,
@@ -141,11 +134,7 @@ addDelicious = function(conf) {
 		type: 'popup'
 		}, function() {
 			chrome.windows.getCurrent(function(window) {
-				chrome.tabs.getSelected(window.id,
-				function (response){
-					ourWindow = response.id
-					// alert('Our window has a tab id of: ' + ourWindow);
-				});
+				getOurWindow();
 			});				
 	});
 	
